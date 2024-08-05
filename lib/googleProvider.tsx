dda6b81b57aed33f30823a46104cadc263671323
Nicolas Romero <nico.rc2@outlook.com>
2024-08-05 00:36:02 -0400

    fixed env variables

--- lib/googleProvider.tsx
+++ lib/googleProvider.tsx
@@ -1,7 +1,7 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 
 const google = createGoogleGenerativeAI({
-  apiKey: process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY,
+  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
   baseURL: "https://generativelanguage.googleapis.com/v1beta",
 });
 
