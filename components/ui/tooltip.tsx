d23f9830ee2857e6853ce1ade2b93bfc8d68b517
Nicolas Romero <nico.rc2@outlook.com>
2024-08-06 07:20:11 -0400

    fixe tooltip

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -18,12 +18,8 @@ const TooltipContent = React.forwardRef<
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
-    // className={cn(
-    //   "z-50 overflow-hidden rounded-md border bg-popover bg-chart_pastelBlue_light  text-white w-[50%] px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-    //   className
-    // )}
     className={cn(
-      " hidden lg:block z-50 overflow-hidden rounded-md bg-popover  border-[1px] border-semantic_green_light/40 text-black min-w-52 max-w-52 px-3 py-1.5 text-sm shadow-md animate-in fade-in-0 ",
+      "z-50 overflow-hidden rounded-md bg-popover bg-chart_pastelBlue_light  text-black min-w-[50%] px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
     {...props}
